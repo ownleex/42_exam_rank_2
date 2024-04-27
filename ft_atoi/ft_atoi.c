a006b0c28c8ddee2ca4fcdbfad69dfca4ce0f19c
ownleex <ownleex@gmail.com>
2024-04-27 04:22:59 +0200

    Corriger la fonction ft_atoi dans ft_atoi.c

--- ft_atoi/ft_atoi.c
+++ ft_atoi/ft_atoi.c
@@ -1,18 +1,15 @@
 int	ft_atoi(const char *str)
 {
-	int	result = 0;
-	int	sign = 1;
+	int		res = 0;
+	int		sign = 1;
 
-	if (*str == ' ' || (*str >= 9 && *str <= 13))
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
 		str++;
 	if (*str == '-')
 		sign = -1;
-	if (*str == '-' || *str == '+')
+	if (*str == '+' || *str == '-')
 		str++;
-	while (*str >= '0' && *str <= '9')
-	{
-		result = result * 10 + *str - '0';
-		str++;
-	}
-	return (result * sign);
+	while (*str >= '0'&& *str <= '9')
+		res = res * 10 + *str++ - '0';
+	return (res * sign);
 }
