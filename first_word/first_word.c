f175d65f84583b64b0c2b55c475302ee0338853e
ownleex <ownleex@gmail.com>
2024-05-04 16:11:24 +0200

    chore: Refactor first_word.c for improved readability and efficiency

--- first_word/first_word.c
+++ first_word/first_word.c
@@ -6,13 +6,10 @@ int	main(int argc, char **argv)
 
 	if (argc == 2)
 	{
-			while (argv[1][i] == ' ' || argv[1][i] == '\t')
-				i++;
-			while ((argv[1][i] != ' ' && argv[1][i] != '\t') && argv[1][i])
-			{
-				write(1, &argv[1][i], 1);
-				i++;
-			}
+		while (argv[1][i] == ' ' || argv[1][i] == '\t')
+			i++;
+		while (argv[1][i] != ' ' && argv[1][i] != '\t' && argv[1][i])
+			write(1, &argv[1][i++], 1);
 	}
 	write(1, "\n", 1);
 	return (0);
