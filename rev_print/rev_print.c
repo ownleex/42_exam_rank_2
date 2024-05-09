e0593118f9d3130c6a937cbf5dc37e44ce0f27a7
ownleex <ownleex@gmail.com>
2024-05-09 18:33:00 +0200

    chore: Refactor rev_print.c for improved readability and efficiency

--- rev_print/rev_print.c
+++ rev_print/rev_print.c
@@ -1,23 +1,15 @@
 #include <unistd.h>
 
-int	ft_strlen(char *str)
+int		main(int argc, char **argv)
 {
-	int i = 0;
-
-	while (str[i])
-		i++;
-	return (i);
-}
-
-int	main(int argc, char **argv)
-{
-	int	len;
+	int		i = 0;
 
 	if (argc == 2)
 	{
-		len = ft_strlen(argv[1]);
-		while (len--)
-			write(1, &argv[1][len], 1);
+		while (argv[1][i])
+			i++;
+		while (--i >= 0)
+			write(1, &argv[1][i], 1);
 	}
 	write(1, "\n", 1);
 	return (0);
