1c9182ace4058aca220961df31ec25ab1ce6aa28
ownleex <ownleex@gmail.com>
2024-05-10 18:57:48 +0200

    Refactor ft_itoa.c for improved readability and efficiency

--- ft_itoa/ft_itoa.c
+++ ft_itoa/ft_itoa.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
-#include <stdio.h>
 
-int	ft_len(int nbr)
+int		ft_len(int nbr)
 {
 	int		len = (nbr <= 0) ? 1 : 0;
 
@@ -13,27 +12,26 @@ int	ft_len(int nbr)
 	return (len);
 }
 
-char *ft_itoa(int nbr)
+char	*ft_itoa(int nbr)
 {
 	int		len = ft_len(nbr);
 	char	*str = (char *)malloc(sizeof(char) * (len + 1));
-	char	*number = "0123456789";
 
 	if (nbr == 0)
 	{
 		str[0] = '0';
-		return str;
+		return (str);
 	}
 	if (nbr < 0)
 	{
 		str[0] = '-';
-		nbr *= -1;
+		nbr = -nbr;
 	}
 	str[len] = '\0';
 	while (nbr != 0)
 	{
 		len--;
-		str[len] = number[nbr % 10];
+		str[len] = nbr % 10 + '0';
 		nbr /= 10;
 	}
 	return (str);
