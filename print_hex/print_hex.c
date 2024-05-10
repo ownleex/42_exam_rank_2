a71839f0fc7b62ddb2ccc29178e0a43a3b80b655
ownleex <ownleex@gmail.com>
2024-05-10 19:31:33 +0200

    chore: Refactor print_hex.c for improved readability and efficiency

--- print_hex/print_hex.c
+++ print_hex/print_hex.c
@@ -1,31 +1,27 @@
 #include <unistd.h>
 
-int	ft_atoi(char *str)
+int		ft_atoi(char *str)
 {
 	int		res = 0;
 
-	while (*str)
+	while (*str >= '0' && *str <= '9')
 		res = res * 10 + *str++ -'0';
 	return (res);
 }
 
 void	print_hex(int nbr)
 {
-	char *str = "0123456789abcdef";
+	char	*number = "0123456789abcdef";
 
 	if (nbr >= 16)
 		print_hex(nbr / 16);
-	write(1, &str[nbr % 16], 1);
+	write(1, &number[nbr % 16], 1);
 }
 
-int	main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
-	int		i = 0;
-
 	if (argc == 2)
-	{
 		print_hex(ft_atoi(argv[1]));
-	}
 	write(1, "\n", 1);
 	return (0);
 }
