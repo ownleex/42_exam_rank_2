1134ca0dbf7efe534a039fcba4179a4116e7765e
ownleex <ownleex@gmail.com>
2024-05-13 15:50:44 +0200

    complete

--- Level 3/paramsum/paramsum.c
+++ Level 3/paramsum/paramsum.c
@@ -2,18 +2,17 @@
 
 void	ft_putnbr(int nbr)
 {
-	char	c;
-
-	if (nbr > 10)
+	if (nbr >= 10)
 		ft_putnbr(nbr / 10);
-	c = nbr % 10 + '0';
+	char	c = nbr % 10 + '0';
 	write(1, &c, 1);
 }
 
-int	main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
 	(void)argv;
-	ft_putnbr(argc - 1);
+	if (argc >= 1)
+		ft_putnbr(argc - 1);
 	write(1, "\n", 1);
 	return (0);
 }
