d046a1f9c3c603db28c0742e6f9302d86b1025dc
ownleex <ownleex@gmail.com>
2024-05-13 18:33:30 +0200

    complete

--- Level 3/pgcd/pgcd.c
+++ Level 3/pgcd/pgcd.c
@@ -3,16 +3,13 @@
 
 int		main(int argc, char **argv)
 {
-	int	nbr1;
-	int	nbr2;
-	int	div = 1;
-	int	pgcd = 0;
-
 	if (argc == 3)
 	{
-		nbr1 = atoi(argv[1]);
-		nbr2 = atoi(argv[2]);
-		while (div <= nbr1 || div <= nbr2)
+		int	nbr1 = atoi(argv[1]);
+		int	nbr2 = atoi(argv[2]);
+		int	div = 1;
+		int	pgcd = 0;
+		while (div < nbr1 && div < nbr2)
 		{
 			if (nbr1 % div == 0 && nbr2 % div == 0)
 				pgcd = div;
