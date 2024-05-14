554e3d3d3d0f84b8db92129c59864c28a8e11f18
ownleex <ownleex@gmail.com>
2024-05-14 22:35:14 +0200

    complete

--- Level 4/ft_itoa/ft_itoa.c
+++ Level 4/ft_itoa/ft_itoa.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 
-int		ft_len(int nbr)
+int		num_len(int	nbr)
 {
 	int		len = (nbr <= 0) ? 1 : 0;
 
@@ -14,24 +14,20 @@ int		ft_len(int nbr)
 
 char	*ft_itoa(int nbr)
 {
-	int		len = ft_len(nbr);
+	int		len = num_len(nbr);
 	char	*str = (char *)malloc(sizeof(char) * (len + 1));
 
 	if (nbr == 0)
-	{
 		str[0] = '0';
-		return (str);
-	}
 	if (nbr < 0)
 	{
-		str[0] = '-';
 		nbr = -nbr;
+		str[0] = '-';
 	}
 	str[len] = '\0';
 	while (nbr != 0)
 	{
-		len--;
-		str[len] = nbr % 10 + '0';
+		str[--len] = nbr % 10 + '0';
 		nbr /= 10;
 	}
 	return (str);
