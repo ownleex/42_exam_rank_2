2b17fac632ee1f7bcf1a2c1650b68efe43808cf6
ownleex <ownleex@gmail.com>
2024-05-15 17:11:55 +0200

    complete

--- Level 4/rostring/rostring.c
+++ Level 4/rostring/rostring.c
@@ -9,31 +9,34 @@ void	rostring(char *str)
 	while (str[start] == ' ' || str[start] == '\t')
 		start++;
 	end = start;
-	while (str[end] != ' ' && str[end] != '\t')
+	while (str[end] && (str[end] != ' ' && str[end] != '\t'))
 		end++;
 	i = end;
 	while (str[i] == ' ' || str[i] == '\t')
 		i++;
-	while (str[i])
+	if (str[i])
 	{
-		if (str[i] == ' ' || str[i] == '\t')
+		while (str[i])
 		{
-			while (str[i] == ' ' || str[i] == '\t')
-				i++;
-			if (str[i])
-				write(1, " ", 1);
+			if (str[i] == ' ' || str[i] == '\t')
+			{
+				while (str[i] == ' ' || str[i] == '\t')
+					i++;
+				if (str[i])
+					write(1, " ", 1);
+			}
+			else
+				write(1, &str[i++], 1);
 		}
-		else
-			write(1, &str[i++], 1);
+		write(1, " ", 1);
 	}
-	write(1, " ", 1);
 	write(1, &str[start], end - start);
 
 }
 
 int		main(int argc, char **argv)
 {
-	if (argc == 2)
+	if (argc > 1)
 		rostring(argv[1]);
 	write(1, "\n", 1);
 	return (0);
