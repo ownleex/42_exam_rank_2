ac9d80b31029f184979498b9332e34dbb33a8dfd
ownleex <ownleex@gmail.com>
2024-05-15 17:22:44 +0200

    complete

--- Level 2/camel_to_snake/camel_to_snake.c
+++ Level 2/camel_to_snake/camel_to_snake.c
@@ -1,22 +1,23 @@
 #include <unistd.h>
 
-int	main(int argc, char **argv)
+void	ft_camel_to_snake(char *str)
 {
-	int i = 0;
-
-	if (argc == 2)
+	while (*str)
 	{
-		while (argv[1][i])
+		if (*str >= 'A' && *str <= 'Z')
 		{
-			if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
-			{
-				argv[1][i] += 32;
-				write(1, "_", 1);
-			}
-			write(1, &argv[1][i], 1);
-			i++;
+			*str += 32;
+			write(1, "_", 1);
 		}
+		else
+			write(1, str++, 1);
 	}
+}
+
+int		main(int argc, char **argv)
+{
+	if (argc == 2)
+		ft_camel_to_snake(argv[1]);
 	write(1, "\n", 1);
 	return (0);
 }
