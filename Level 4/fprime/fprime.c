b8893ea1be588133e7621a0113a7944e69a76ac4
ownleex <ownleex@gmail.com>
2024-05-15 23:19:42 +0200

    complete

--- Level 4/fprime/fprime.c
+++ Level 4/fprime/fprime.c
@@ -1,26 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void	fprime(int nbr)
+{
+	int		i = 2;
+
+	while (nbr >= i)
+	{
+		if (nbr % i == 0)
+		{
+			printf("%d", i);
+			if (nbr == i)
+				break;
+			printf("*");
+			nbr /= i;
+			i = 1;
+		}
+		i++;
+	}
+}
+
 int		main(int argc, char **argv)
 {
 	if (argc == 2)
-	{
-		int		nbr = atoi(argv[1]);
-		int		i = 1;
-
-		while (++i <= nbr)
-		{
-			if (nbr % i == 0)
-			{
-				printf("%d", i);
-				if (nbr == i)
-					break;
-				printf("*");
-				nbr /= i;
-				i = 1;
-			}
-		}
-	}
+		fprime(atoi(argv[1]));
 	printf("\n");
 	return (0);
 }
