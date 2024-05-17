fb8748b4ab4b0c2574fedb927301cbf831369526
ownleex <ownleex@gmail.com>
2024-05-17 18:28:14 +0200

    complete

--- Level 3/expand_str/expand_str.c
+++ Level 3/expand_str/expand_str.c
@@ -1,28 +1,31 @@
 #include <unistd.h>
 
+void	expand_str(char *str)
+{
+	int		i		= 0;
+	int		end		= 0;
+
+	while (str[end])
+		end++;
+	while (str[end] == ' ' || str[end] == '\t' || str[end] == '\0')
+		end--;
+	while (str[i] == ' ' || str[i] == '\t')
+		i++;
+	while (str[i] && i <= end)
+	{
+		if (str[i] != ' ' && str[i] != '\t')
+			write(1, &str[i], 1);
+		if (str[i] == ' ' || str[i] == '\t')
+			if (str[i + 1] && str[i + 1] != ' ' && str[i + 1] != '\t')
+				write(1, "   ", 3);
+		i++;
+	}
+}
+
 int		main(int argc, char **argv)
 {
-	int		i = 0;
-	int		end = 0;
-
 	if (argc == 2)
-	{
-		while (argv[1][end])
-			end++;
-		while (argv[1][end] == ' ' || argv[1][end] == '\t' || argv[1][end] == '\0')
-			end--;
-		while (argv[1][i] == ' ' || argv[1][i] == '\t')
-			i++;
-		while (argv[1][i] && i <= end)
-		{
-			if (argv[1][i] != ' ' && argv[1][i] != '\t')
-				write(1, &argv[1][i], 1);
-			if (argv[1][i] == ' ' || argv[1][i] == '\t')
-				if (argv[1][i + 1] != ' ' && argv[1][i + 1] != '\t')
-					write(1, "   ", 3);
-			i++;
-		}
-	}
+		expand_str(argv[1]);
 	write(1, "\n", 1);
 	return (0);
 }
