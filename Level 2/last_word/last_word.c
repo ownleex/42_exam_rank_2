812188ca17dfe192d03bea682ab38fc54377edcb
ownleex <ownleex@gmail.com>
2024-05-18 23:35:15 +0200

    complete

--- Level 2/last_word/last_word.c
+++ Level 2/last_word/last_word.c
@@ -1,25 +1,23 @@
 #include <unistd.h>
 
-int	main(int argc, char **argv)
+void	last_word(char *str)
 {
-	int	len = 0;
+	while (*str)
+		str++;
+	str--;
+	while ((*str == ' ' || *str == '\t') && *str)
+		str--;
+	while (*str != ' ' && *str != '\t' && *str)
+		str--;
+	str++;
+	while (*str != ' ' && *str != '\t' && *str)
+		write(1, str++, 1);
+}
 
+int		main(int argc, char **argv)
+{
 	if (argc == 2)
-	{
-		while (argv[1][len])
-			len++;
-		len--;
-		while ((argv[1][len] == ' ' || argv[1][len] == '\t') && argv[1][len])
-			len--;
-		while ((argv[1][len] != ' ' && argv[1][len] != '\t') && argv[1][len])
-			len--;
-		len++;
-		while ((argv[1][len] != ' ' && argv[1][len] != '\t') && argv[1][len])
-		{
-			write(1, &argv[1][len], 1);
-			len++;
-		}
-	}
+		last_word(argv[1]);
 	write(1, "\n", 1);
 	return (0);
 }
