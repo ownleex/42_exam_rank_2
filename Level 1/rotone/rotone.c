2a5f58c1502aa9700dbd68849cd341dda7f8b007
ownleex <ownleex@gmail.com>
2024-05-20 14:48:54 +0200

    complete

--- Level 1/rotone/rotone.c
+++ Level 1/rotone/rotone.c
@@ -1,20 +1,21 @@
 #include <unistd.h>
 
+void	rotone(char *str)
+{
+	while (*str)
+	{
+		if ((*str >= 'a' && *str <= 'y') || (*str >= 'A' && *str <= 'Y'))
+			*str += 1;
+		else if (*str == 'z' || *str == 'Z')
+			*str -= 25;
+		write(1, str++, 1);
+	}
+}
+
 int		main(int argc, char **argv)
 {
-	int	i = 0;
-
 	if (argc == 2)
-	{
-		while (argv[1][i])
-		{
-			if ((argv[1][i] >= 'a' && argv[1][i] <= 'y') || (argv[1][i] >= 'A' && argv[1][i] <= 'Y'))
-				argv[1][i] += 1;
-			else if (argv[1][i] == 'z'|| argv[1][i] == 'Z')
-				argv[1][i] -= 25;
-			write(1, &argv[1][i++], 1);
-		}
-	}
+		rotone(argv[1]);
 	write(1, "\n", 1);
 	return (0);
 }
