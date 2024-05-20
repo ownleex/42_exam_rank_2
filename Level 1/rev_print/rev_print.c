0f54344706d5df44aae9a589e7a39674e58bda3d
ownleex <ownleex@gmail.com>
2024-05-20 16:27:37 +0200

    complete

--- Level 1/rev_print/rev_print.c
+++ Level 1/rev_print/rev_print.c
@@ -1,16 +1,18 @@
 #include <unistd.h>
 
+void	rev_print(char *str)
+{
+	while (*str)
+		str++;
+	str--;
+	while (*str)
+		write(1, str--, 1);
+}
+
 int		main(int argc, char **argv)
 {
-	int		i = 0;
-
 	if (argc == 2)
-	{
-		while (argv[1][i])
-			i++;
-		while (--i >= 0)
-			write(1, &argv[1][i], 1);
-	}
+		rev_print(argv[1]);
 	write(1, "\n", 1);
 	return (0);
 }
