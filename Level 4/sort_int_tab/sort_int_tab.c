249881943a61a02f6014a291df8e53d5bc7a2695
ownleex <ownleex@gmail.com>
2024-05-22 01:29:22 +0200

    complete

---
+++ Level 4/sort_int_tab/sort_int_tab.c
@@ -0,0 +1,24 @@
+void	ft_swap(int	*a, int *b)
+{
+	int	tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void	sort_int_tab(int *tab, unsigned int size)
+{
+	unsigned int	i = 0;
+
+	while (i < (size - 1))
+	{
+		if (tab[i] > tab[i + 1])
+		{
+			ft_swap(&tab[i], &tab[i + 1]);
+			i = 0;
+		}
+		else
+			i++;
+	}
+}
