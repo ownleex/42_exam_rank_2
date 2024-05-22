24ab6b8044e83b02ce617812d856e7f563da4ae0
ownleex <ownleex@gmail.com>
2024-05-22 03:25:10 +0200

    complete

--- Level 2/reverse_bits/reverse_bits.c
+++ Level 2/reverse_bits/reverse_bits.c
@@ -1,13 +1,13 @@
 unsigned char	reverse_bits(unsigned char octet)
 {
-	int		i = 8;
+	int				bit = 8;
 	unsigned char	res = 0;
 
-	while (i > 0)
+	while (bit > 0)
 	{
 		res = res * 2 + (octet % 2);
-		octet = octet / 2;
-		i--;
+		octet /= 2;
+		bit--;
 	}
 	return (res);
 }
