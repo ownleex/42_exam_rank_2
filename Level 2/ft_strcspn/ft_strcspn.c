545b20f9874a5a8bef819878433d05f0f6f932ad
ownleex <ownleex@gmail.com>
2024-05-22 20:31:17 +0200

    complete

--- Level 2/ft_strcspn/ft_strcspn.c
+++ Level 2/ft_strcspn/ft_strcspn.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
+#include <stdlib.h>
 
-size_t  ft_strcspn(const char *s, const char *reject)
+size_t      ft_strcspn(const char *s, const char *reject)
 {
-    int     i = 0;
-    int     j = 0;
+	size_t		i = 0;
+	int			j = 0;
 
-    while (s[i] != '\0')
-    {
-	    j = 0;
-        while (reject[j] != '\0')
-        {
-            if(s[i] == reject[j])
-                return (i);
-            j++;
-        }
-        i++;
-    }
-    return (i);
+	while (s[i])
+	{
+		j = 0;
+		while (reject[j])
+		{
+			if (s[i] == reject[j])
+				return (i);
+			j++;
+		}
+		i++;
+	}
+	return (i);
 }
